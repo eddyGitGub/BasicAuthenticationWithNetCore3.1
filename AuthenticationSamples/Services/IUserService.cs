3f28d7a04dbb1a8538f902d1ae2ee05e6f88376c
eddygitgub <eddycase1@gmail.com>
2020-05-28 10:52:22 +0100

    basic authentication

---
+++ AuthenticationSamples/Services/IUserService.cs
@@ -0,0 +1,12 @@
+﻿using AuthenticationSamples.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace AuthenticationSamples.Services
+{
+    public interface IUserService
+    {
+        Task<User> Authenticate(string username, string password);
+        Task<IEnumerable<User>> GetAll();
+    }
+}
